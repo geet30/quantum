105515dfc6ab8e17fe112e61115eb8d5906bcd9e
geetanjali <geetanjali.crebos@gmail.com>
2022-11-24 16:18:06 +0530

    allow headers

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -20,6 +20,6 @@ class Cors
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Credentials', 'true')
             ->header('Access-Control-Max-Age', '10000')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With','*');
+            ->header('Access-Control-Allow-Headers', '*');
     }
 }
