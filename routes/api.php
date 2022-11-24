2f04ad544392d41c5871fb881b48368272ed4540
geetanjali <geetanjali.crebos@gmail.com>
2022-11-24 12:24:56 +0530

    change to api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\V1\Vaults\VaultsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+$router->group(['middleware' => 'cors','prefix' => 'vaults'], function () use ($router) {
+    $router->get('/', [VaultsController::class, 'getvaults']);
+    $router->get('detail/graph/{id}', [VaultsController::class, 'getVaultGraph']);
+    $router->get('detail/asset_info/{id}', [VaultsController::class, 'getVaultAssets']);
+    $router->get('detail/transaction_detail/{id}', [VaultsController::class, 'getVaultTransaction']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
